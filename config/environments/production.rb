6c2ff0dea99dfca7b638649b19b6b3ceb4dc3c38
bdcinf <shubhendra.p@bdcinfotech.com>
2015-12-02 09:54:56 +0530

    asd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,6 @@ Antiques::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'actionumisphil.com' }
 
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
