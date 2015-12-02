4d2ff6d11c103f41c19844b1edb1da66971c10dc
bdcinf <shubhendra.p@bdcinfotech.com>
2015-12-02 12:08:32 +0530

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,16 +7,17 @@ Antiques::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'actionumisphil.com' }
 
+  config.action_mailer.default_url_options = { :host => 'actionumisphil.com' }
+  config.action_mailer.delivery_method = :smtp
+  
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
     :user_name            => "bdcatpl@gmail.com",
     :password             => "bdcatpl@12345",
-    :authentication       => "plain",
+    :authentication       => "login",
     :enable_starttls_auto => true
   }
 
